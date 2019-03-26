93da09671e9b90098e6987abc4f5603b6014ad01
aleMainas <alessandro.mainas@mambo.pe>
2019-03-26 11:58:04 -0500

    Adds service worker
    Co-Authored-By: Alessandro Mainas <Bortoman@users.noreply.github.com>

---
+++ sw.js
@@ -0,0 +1,84 @@
+var staticCacheName = 'bortoman-content';
+var contentImgsCache = 'bortoman-content-imgs';
+var secondaryCache ='bortoman-secondary-content';
+var allCaches = [
+  staticCacheName,
+  contentImgsCache,
+  secondaryCache
+];
+
+self.addEventListener('install', function(event) {
+  console.log('installing...');
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/index.html',
+        '/Security.html',
+        'css/Style.css',
+        'css/StyleSec.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('bortoman-') &&
+                 !allCaches.includes(cacheName);
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  var requestUrl = new URL(event.request.url);
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname === '/') {
+      event.respondWith(caches.match('/index.html'));
+      return;
+    }
+    if (requestUrl.pathname.startsWith('/img/')) {
+      event.respondWith(servePhoto(event.request));
+      return;
+    }
+  }
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        var fetchRequest = event.request.clone();
+        return fetch(fetchRequest).then(function(response) {
+            var responseToCache = response.clone();
+            // Caching secondary pages
+            caches.open(secondaryCache).then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      })
+    );
+});
+
+
+function servePhoto(request) {
+  var storageUrl = request.url;
+  console.log('serving photos...');
+  return caches.open(contentImgsCache).then(function(cache) {
+    return cache.match(storageUrl).then(function(response) {
+      if (response) return response;
+      return fetch(request).then(function(networkResponse) {
+        cache.put(storageUrl, networkResponse.clone());
+        return networkResponse;
+      });
+    });
+  });
+}
